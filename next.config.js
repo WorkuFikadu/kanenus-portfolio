f245823804e688ceab48e8683f33d2c95d206f89
Kanenus Kasa Bayisa <kanenus@example.com>
2026-09-02 19:10:09 +0300

    Configure static export support for InfinityFree and static hosting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  experimental: {
+    cpus: 1,
+    workerThreads: false,
+  },
 }
 
 module.exports = nextConfig
